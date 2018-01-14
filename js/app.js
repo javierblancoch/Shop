129481fea7582f5ec2a75a4a102bec44a50c1282
javier164 <javieresmithblanc@gmail.com>
2018-01-14 00:50:32 -0500

    start

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const $form = $('#suscribeForm')
+const $formGroup = $form.find('.form-group')
+const $formControl = $formGroup.find('.form-control')
+
+$form.submit((ev) => {
+  ev.preventDefault()
+  
+  const error = Math.random() > 0.5
+  $formGroup.removeClass().addClass('form-group')
+  $formControl.removeClass().addClass('form-control')
+  $formGroup.addClass(error ? 'has-danger' : 'has-success')
+  $formControl.addClass(error ? 'form-control-danger' : 'form-control-success')
+  $formGroup.find('.form-control-feedback').remove()
+  var el;
+  if (error) {
+    el = $('<div>', { html: 'Ha ocurrido un error' })
+  } else {
+    el = $('<div>', { html: 'Te enviaremos todas las novedades!' })
+  }
+  el.addClass('form-control-feedback')
+  $formGroup.append(el)
+})
+
+function mostrarModal() {
+  if(localStorage.noMostrarModal==undefined){
+   localStorage.noMostrarModal = false 
+  }
+  if (!JSON.parse(localStorage.noMostrarModal)) {
+    $('#modalOferta').modal()
+  }
+
+  $('#btnNoRegistrar').click((ev) => {
+    localStorage.noMostrarModal = true
+  })
+}
+
+const $filtrosToggle = $('#filtrosToggle')
+$filtrosToggle.click(function (ev){
+  ev.preventDefault()
+
+  const $i = $filtrosToggle.find('i.fa')
+  const isDown = $i.hasClass('fa-chevron-down')
+  if(isDown){
+    $i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+  } else {
+    $i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+  }
+})
